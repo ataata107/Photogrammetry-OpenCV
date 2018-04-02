563621c7efeb7b90cf577180ee3e146e963d026b
Shazeb <ataata107@gmail.com>
2018-04-02 14:28:01 +0530

    Day 2

--- colourRange.py
+++ colourRange.py
@@ -1,5 +1,6 @@
 import cv2
 from operator import xor
+import numpy as np
 
 def callback(value):
     pass
@@ -29,13 +30,18 @@ def get_trackbar_values(range_filter):
 def main():
 
     range_filter = 'BGR'
-    image=cv2.imread('for_assignment/DSC01798.JPG')                            
+    image=cv2.imread('for_assignment/DSC02426.JPG')                            
     image = cv2.resize(image, (1000, 1000))
+    #image = cv2.addWeighted(image,2,np.zeros(image.shape,image.dtype),0,50)
+    image = cv2.pyrMeanShiftFiltering(image,31,51)
     setup_trackbars(range_filter)
+    frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+
 
     while True:
 
-        frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        
         v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
 
         thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
--- test.py
+++ test.py
@@ -1,17 +1,23 @@
 import cv2
 import numpy as np
-Lower = (91, 0, 182)
-Upper = (150, 51, 236)
+Lower = (59, 0, 119)
+Upper = (255, 255, 255)
 
 
-img=cv2.imread('for_assignment/DSC01798.JPG')
-img = cv2.resize(img, (1000, 1000))
+img=cv2.imread('for_assignment/DSC02407.JPG')
+
+#img = cv2.resize(img, (1000, 1000))
+#img = cv2.addWeighted(img,2,np.zeros(img.shape,img.dtype),0,50)
+blurred = cv2.pyrMeanShiftFiltering(img,21,51)
+
 kernel = np.ones((15,15), np.float32)/225
 #smoothed = cv2.filter2D(img,-1,kernel)
 #img = cv2.GaussianBlur(img, (11, 11), 0)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv, Lower, Upper)
-edges = cv2.Canny(mask.copy(), 50, 150, apertureSize = 3 )
+#mask = cv2.erode(mask, None, iterations = 2)
+#mask = cv2.dilate(mask, None, iterations = 2)
+edges = cv2.Canny(mask.copy(), 50, 150, apertureSize = 5)
 #laplacian = cv2.Laplacian(mask,cv2.CV_64F)
 sobelx = cv2.Sobel(mask,cv2.CV_64F,1,0,ksize=9)
 sobely = cv2.Sobel(mask,cv2.CV_64F,0,1,ksize=9)
@@ -21,16 +27,32 @@ kernel = np.ones((1,1), np.uint8)
 # Creating the kernel for dilation
 #dilated_image = cv2.dilate(laplacian,kernel,iterations=1)
 #ret, threshold = cv2.threshold(laplacian,2,255,cv2.THRESH_BINARY)
-#(cnts, _) = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+th,cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+print(len(cnts),"length")
+#cv2.drawContours(img,cnts,-1,(0,0,255),2)
+sorted_ctrs = sorted(cnts, key=lambda ctr: cv2.boundingRect(ctr)[0])
+for i, ctr in enumerate(sorted_ctrs):
+    # Get bounding box
+    x, y, w, h = cv2.boundingRect(ctr)
+ 
+    # Getting ROI
+    roi = mask[y:y+h, x:x+w]
+    roi = cv2.resize(roi, (100, 100))
+ 
+    # show ROI
+    cv2.imshow('segment no:'+str(i),roi)
+    #cv2.rectangle(img,(x,y),( x + w, y + h ),(0,255,0),1)
+    #cv2.waitKey(0)
 #cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
 #edges = cv2.Canny(mask, 50, 50)
 i, j = edges.shape
 print (i)
 print (j)
-lines = cv2.HoughLines(edges, 1, np.pi/180, 5)
+'''
+lines = cv2.HoughLines(roi, 1, np.pi/180, 7)
 print(lines)
 if lines is not None:
-#print len(lines)
+    print (len(lines))
     for line in range(len(lines)):
         for rho,theta in lines[line]:
             #print (lines)
@@ -42,24 +64,26 @@ if lines is not None:
              y1 = int(y0 + 1000*(a))
              x2 = int(x0 - 1000*(-b))
              y2 = int(y0 - 1000*(a))
-             cv2.line(img,(x1,y1),(x2,y2),(0,0,255),1)
+             cv2.line(roi,(x1,y1),(x2,y2),(0,0,255),1)
+'''
 
 #cv2.imwrite('houghlines3.jpg',img)
-#corners = cv2.goodFeaturesToTrack(laplacian,5,0.01,10)
-#corners = np.int0(corners)
-#for corner in corners:
-#    x,y = cornel.ravel()
-#    cv2.circle(laplacian,(x,y),3,255,-1)
+corners = cv2.goodFeaturesToTrack(edges,5,0.01,100)
+corners = np.int0(corners)
+print("corn",corners)
+
+for corner in corners:
+    x,y = corners.ravel()
+    cv2.circle(mask,(x,y),3,255,-1)
 
 #blurred = cv2.GaussianBlur(mask, (11, 11), 0)
-'''
-mask = cv2.dilate(mask, None, iterations = 2)
-mask = cv2.erode(mask, None, iterations = 2)
-'''
 
-#cv2.imshow('lapl', img)
-cv2.imshow('sobx', sobelx)
-cv2.imshow('edges', edges)
-cv2.imshow('mask', mask)
+
+
+
+cv2.imshow('lapl', roi)
+#cv2.imshow('sobx', sobelx)
+#cv2.imshow('edges', edges)
+#cv2.imshow('edges', edges)
 
  
